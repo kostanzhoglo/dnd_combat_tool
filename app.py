36e671b4b8b18809951789d54166b402b7a7bb1f
kostanzhoglo <jeffreywithers@gmail.com>
2020-09-17 17:11:24 -0400

    Put heroku prod URI back

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + str(OWNER) + ':' + str(DB_PASSWORD) + '@localhost/dnd'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = str(HEROKU_URI)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://plwquqdmkziorw:b1bc4f04549868c5495efde4b62dbfa1c13f8eded2f218eab3f9078bd80e8089@ec2-54-91-178-234.compute-1.amazonaws.com:5432/d7tk3fs4asvj52'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
